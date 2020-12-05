77e27144d57ed6f6dbce1d13c0c8f09d554eb9d9
Yizhe.Zhang <enochyizhe@foxmail.com>
2020-12-05 08:51:37 +0800

    adjust card view more simple

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -22,7 +22,6 @@ struct EmojiMemoryGameView: View {
         }
             .foregroundColor(.orange)
             .padding()
-            .font(Font.largeTitle)
     }
 }
 
@@ -30,15 +29,30 @@ struct CardView: View {
     var card : MemoryGame<String>.Card
     
     var body: some View {
+        GeometryReader { geometry in
+            self.body(for: geometry.size)
+        }
+    }
+    
+    func body(for size: CGSize) -> some View {
         ZStack {
             if card.isFaceUp {
-                RoundedRectangle(cornerRadius: 10).fill(Color.white)
-                RoundedRectangle(cornerRadius: 10).stroke()
+                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
                 Text(card.content)
             } else {
-                RoundedRectangle(cornerRadius: 10).fill()
+                RoundedRectangle(cornerRadius: cornerRadius).fill()
             }
         }
+        .font(Font.system(size: fontSize(for: size)))
+    }
+    
+    // MARK: - Draw Constants
+    
+    let cornerRadius : CGFloat = 10.0
+    let edgeLineWidth: CGFloat = 3.0
+    func fontSize(for size: CGSize) -> CGFloat {
+        min(size.width, size.height) * 0.75
     }
 }
 
