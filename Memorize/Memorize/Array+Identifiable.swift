a2943207a12dc953afc3cd536fa8f25d1d76180e
Yizhe.Zhang <enochyizhe@foxmail.com>
2020-12-05 17:41:57 +0800

    Using enum to fix error index problem

--- Memorize/Memorize/Array+Identifiable.swift
+++ Memorize/Memorize/Array+Identifiable.swift
@@ -10,13 +10,13 @@ import Foundation
 
 extension Array where Element : Identifiable {
     
-    func firstIndex(matching: Element) -> Int {
+    func firstIndex(matching: Element) -> Int? {
         for index in 0..<self.count {
             if self[index].id == matching.id {
                 return index
             }
         }
-        return 0
+        return nil
     }
     
 }
--- Memorize/Memorize/Grid.swift
+++ Memorize/Memorize/Grid.swift
@@ -32,6 +32,6 @@ struct Grid<Item, ItemView>: View where Item : Identifiable, ItemView : View {
         let index = items.firstIndex(matching: item)
         return viewForItem(item)
             .frame(width: layout.itemSize.width, height: layout.itemSize.height)
-            .position(x: layout.location(ofItemAt: index).x, y: layout.location(ofItemAt: index).y)
+            .position(layout.location(ofItemAt: index!))
     }
 }
